67b82d1984e08015fac1047c6ba1b02c477110ee
Joseph <jrichardson@jmstudios.net>
2017-02-05 11:40:38 -0800

    got the data just need to format it beautifully now

--- server.js
+++ server.js
@@ -3,9 +3,15 @@ var path = require( 'path' );
 var bodyParser = require('body-parser');
 var Promise = require('bluebird');
 var request = require('request');
+var _ = require('underscore');
 
 var app = express();
 var router = express.Router();
+
+var API_KEY = 'AIzaSyCWhwRupMs7IeE4IrGEgHtT0Nt-IGZnP9E';
+var endURL = '&key='+ API_KEY;
+var baseURL = 'https://www.googleapis.com/civicinfo/v2/representatives?address=';
+//example : https://www.googleapis.com/civicinfo/v2/representatives?address=1184+normandy+drive+cambpell+ca+95008&key={YOUR_API_KEY}
 
 router.get( '/', function( req, res ) {
 
@@ -16,13 +22,23 @@ router.get( '/', function( req, res ) {
 
 router.post( '/get-vote-info', function( req, res ) {
 
-    var voterInfoPromise = new Promise(function ( resolve, reject ) {
+    // var voterInfoPromise = new Promise(function ( resolve, reject ) {
+    //
+    // });
+    var address = req.body.address.split(' ').join('+');
+    var fullURL = baseURL+address+endURL;
 
+    console.log( fullURL);
+    console.log( '_____________________________' );
+    request(fullURL, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log("NAILED IT\n\n");
+            console.log(body); // Show the HTML for the Google homepage.
+            console.log("NAILED IT\n\n");
+        }
     });
 
 
-    console.log( req.body.address);
-    console.log( '_____________________________' );
     res.send({'data':'success'})
 });
 
